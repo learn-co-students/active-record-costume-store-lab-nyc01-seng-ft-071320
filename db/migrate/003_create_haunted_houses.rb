427f6cf81022776aca3eb58ecc6a262015b04576
kevin_xie <xie.kevin.j@gmail.com>
2020-07-25 22:49:15 -0400

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,19 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    # By convention, the class name should match the part of the
+    # file name after the number, so in this case:
+    # 002_create_costumes.rb becomes class CreateCostumes
+    def change 
+        create_table :haunted_houses do |t|
+        t.string :name 
+        t.string :location
+        t.string :theme
+        t.float :price
+        t.boolean :family_friendly
+        t.datetime :opening_date
+        t.datetime :closing_date
+        t.text :description
 
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+        t.timestamps null: false
+        end
+    end
+end
